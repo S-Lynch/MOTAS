ed637a8bdff65669470ae8e900a251543e4f352a
Steve <steve_lynch_6@hotmail.com>
2011-05-29 17:47:30 +0100

    rake

--- app/mailers/mot_mailer.rb
+++ app/mailers/mot_mailer.rb
@@ -1,9 +1,9 @@
 class MotMailer < ActionMailer::Base
   default :from => "motasreminder@gmail.com"
 
-  def mot_reminder(user, mot)
-    @user = user
-    @mot = mot
+  def mot_reminder()
+  #  @user = user
+#    @mot = mot
     @send = Mot.joins(:users)
     #mail(:to => user.email, :subject => "Your M.O.T renewal is due")
     mail(:to => "#{@user.name} <#{@user.email}>", :subject => "Your email is due for renewal soon")
