8116aeb146f9c38116c3d9290012cc348c2a2fb9
Steve <steve_lynch_6@hotmail.com>
2011-06-04 20:45:08 +0100

    rake

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,10 +49,9 @@ class User < ActiveRecord::Base
  #@motd = mot.mot_date.to_s
  #@motdate = Date.parse(mot.mot_date.to_s)
  #@days = (@motdate - @today).to_i
- puts @days
  #@days = @diff.to_i
  #  if ((@days < 7) && (@days >= 0))
-  if (Date.today + 7) < mot.mot_date
+ if (Date.today + 7.days) < mot.mot_date
       @thisuser = User.where("id = ?", mot.user_id)
 #      if mot.mot_date-1.day < Time.now.utc
 #        #send e-mail
