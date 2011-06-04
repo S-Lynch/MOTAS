5f84bd4649a3da472b4bf2b74d61fa52f5c5a995
Steve <steve_lynch_6@hotmail.com>
2011-06-04 21:37:11 +0100

    rake

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,5 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
 $(function() {
-  $("#mot_mot_date").datepicker();
+  $("#mot_mot_date").datepicker({dateFormat: 'yy/mm/dd'});
 });
