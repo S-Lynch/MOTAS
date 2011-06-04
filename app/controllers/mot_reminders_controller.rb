e0e6ed7b3a07867e95cbc90940575ea87c6ffa0f
Steve <steve_lynch_6@hotmail.com>
2011-06-04 17:01:07 +0100

    rake

--- app/controllers/mot_reminders_controller.rb
+++ app/controllers/mot_reminders_controller.rb
@@ -1,22 +1,22 @@
 class MotRemindersController < ApplicationController
 
-  def MotReminders.check_mots
-    require 'time'
-
-    
-#    @mots = Mot.where(Time.now.utc, Time.now.utc+1.week )
-# #   @mots = Mot.find(:all, :order => "date")
+#  def MotReminders.check_mots
+#    require 'time'
 #
-#    #iterate through each mot abd do the following
-#    @mots.each do |mot|
-  User.joins(:mots)
-    #  if mot.mot_date-1.day < Time.now.utc
-        #send e-mail
-      MotMailer.mot_reminder(@mot).deliver #or (@user) ?
-
-  #    elsif mot.mot_date-1.day < Time.now.utc
-
-      end
-    end
-#  end
-#end
+#
+##    @mots = Mot.where(Time.now.utc, Time.now.utc+1.week )
+## #   @mots = Mot.find(:all, :order => "date")
+##
+##    #iterate through each mot abd do the following
+##    @mots.each do |mot|
+#  User.joins(:mots)
+#    #  if mot.mot_date-1.day < Time.now.utc
+#        #send e-mail
+#      MotMailer.mot_reminder(@mot).deliver #or (@user) ?
+#
+#  #    elsif mot.mot_date-1.day < Time.now.utc
+#
+#      end
+#    end
+##  end
+end
